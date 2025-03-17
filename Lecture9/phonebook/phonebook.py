af5e0761a28c34ffecd7c5cbd398ce73abbbd7fa
BorreBuu <annukka.kinnari@gmail.com>
2025-03-17 19:39:08 +0200

    added the base code for phonebook and code for exercise_counter

---
+++ Lecture9/phonebook/phonebook.py
@@ -0,0 +1,9 @@
+class PhoneBook:
+    def __init__(self):
+        self.__persons = []
+
+    def add_number(self, name: str, number: str):
+        pass
+
+    def get_number(self, name: str):
+        pass
